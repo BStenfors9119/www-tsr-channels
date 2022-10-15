b11b1fdf274eeaecc864d574664f50de43b00f08
Bryan Stenfors <BStenfors@SodaPopSystems.com>
2022-10-14 18:02:15 -0700

    Updating the back end to handle a city provider not currently active 1

--- src/listings/listings.js
+++ src/listings/listings.js
@@ -4,7 +4,7 @@ export const load = (providerInfo) => {
     return new Promise(async (res, rej) => {
         let date = new Date();
         let tzOffset = date.getTimezoneOffset().toString();
-
+        providerInfo['tzOffset'] = tzOffset;
         const forecastResp = await axios.post(`https://api.thesportsremote.com/api/games/forecast`, {
             forecastInfo: providerInfo
         })
